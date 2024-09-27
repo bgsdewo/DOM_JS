967fd2e4c5a953a36a6613c3b579921fd66dc48b
unknown <bgsdewoo20@gmail.com>
2024-09-27 08:56:12 +0700

    Melihat Isi Document Object Model

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //topic satu Apa itu Dom
 // ini berupa file gambar saja dokumentasinya
+
+// //Topic tiga Melihat Isi Document Object Model
+
+//melihat isi dari document html
+// console.log(document);
+
+//melihat isi dari semua document dengan indeks ke 11
+// console.log(document.all[11]);
+
+// //mengubah cat menjadi bird
+// console.log((document.all[11].innerText = "Bird"));
+
+//melihat isi document indeks ke 10 dengan melihat innerTextnya
+// console.log(document.all[10].innerText);
