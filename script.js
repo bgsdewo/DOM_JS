86cd6f0ef145aab171ee50596b4482f0693c0b0a
unknown <bgsdewoo20@gmail.com>
2024-10-02 09:59:46 +0700

    Menjalankan Event Melalui Property Dom

--- script.js
+++ script.js
@@ -3,3 +3,26 @@
 // hanya berisi gambar saja
 
 //topic dua Menjalankan Event Pada Inline Element
+
+//topic tiga Menjalankan Event Melalui Property Dom
+
+// const buttn = document.querySelector("#clickme");
+
+// //cara ini tidak disarankan karena button akan dieksekusi terlebih dahulu!
+// // buttn.onclick = console.log("telah ditekan");
+
+// //cara ini baru benar!
+// buttn.onclick = function () {
+//   console.log("Telah ditekan");
+// };
+// // buttn.onmouseover = function () {
+// //     console.log("Jangan sentuh aku!");
+// // };
+
+// //ada cara baru juga disimpan dalam variabel
+// const scream = () => {
+//   console.log("Jangan sentuh aku!");
+// };
+// buttn.onmouseover = scream;
+
+// //catatan event onclick bukan hanya dimiliki oleh button saja!
