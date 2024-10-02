e7120aca7941ea3943e2102f8f92917696f570aa
unknown <bgsdewoo20@gmail.com>
2024-10-02 20:07:02 +0700

    Mengenal Event Delegation

--- script.js
+++ script.js
@@ -183,21 +183,45 @@
 // });
 
 // topic sebelasMengenal Event Bubbling
-const button = document.querySelector("#changeColor");
-const container = document.querySelector("#container");
+// const button = document.querySelector("#changeColor");
+// const container = document.querySelector("#container");
 
-button.addEventListener("click", function (e) {
-  container.style.backgroundColor = generateRandomColor();
-  e.stopPropagation();
+// button.addEventListener("click", function (e) {
+//   container.style.backgroundColor = generateRandomColor();
+//   e.stopPropagation();
+// });
+
+// container.addEventListener("click", function () {
+//   container.classList.toggle("hide");
+// });
+
+// const generateRandomColor = () => {
+//   const r = Math.floor(Math.random() * 255);
+//   const g = Math.floor(Math.random() * 255);
+//   const b = Math.floor(Math.random() * 255);
+//   return `rgb(${r} ${g} ${b})`;
+// };
+
+// topic sebelasMengenal Event Delegation
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const list = document.querySelector("#notes");
+
+form.addEventListener("submit", function (e) {
+  //mencegah untuk tidak berpindah halaman(behavier)=kebiasaan
+  e.preventDefault();
+  const noteValue = input.value;
+  const newList = document.createElement("li");
+  newList.innerText = noteValue;
+  list.append(newList);
+  //   console.log(newList);
+  input.value = "";
 });
 
-container.addEventListener("click", function () {
-  container.classList.toggle("hide");
+list.addEventListener("click", (e) => {
+  // console.log(e)
+  // console.dir(e.target)
+  //   e.target.remove();
+  e.target.nodeName === "LI" && e.target.remove();
 });
-
-const generateRandomColor = () => {
-  const r = Math.floor(Math.random() * 255);
-  const g = Math.floor(Math.random() * 255);
-  const b = Math.floor(Math.random() * 255);
-  return `rgb(${r} ${g} ${b})`;
-};
