b50de54848ca751913e0f640c43284a947394a5d
unknown <bgsdewoo20@gmail.com>
2024-10-03 07:34:52 +0700

    Fix Game and Finish DOM

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const p1Button = document.querySelector("#p1-button");
 const p2Button = document.querySelector("#p2-button");
+const resetButton = document.querySelector("#reset");
+const playTo = document.querySelector("#playto");
+
 const p1Display = document.querySelector("#p1-display");
 const p2Display = document.querySelector("#p2-display");
-const resetButton = document.querySelector("#reset");
-const winPointOption = document.querySelector("#windpoint");
 
 let p1Score = 0;
 let p2Score = 0;
-let winPoint = 3;
+let winningScore = 0;
 let isGameOver = false;
+
+function disableButton() {
+  if (winningScore == 0) {
+    p1Button.setAttribute("disabled", "");
+    p2Button.setAttribute("disabled", "");
+  } else {
+    p1Button.removeAttribute("disabled");
+    p2Button.removeAttribute("disabled");
+  }
+}
 
 function reset() {
   isGameOver = false;
@@ -16,13 +27,16 @@ function reset() {
   p2Score = 0;
   p1Display.textContent = 0;
   p2Display.textContent = 0;
+  p1Display.classList.remove("text-success");
+  p2Display.classList.remove("text-success");
 }
 
 p1Button.addEventListener("click", function () {
   if (!isGameOver) {
     p1Score += 1;
-    if (p1Score === winPoint) {
+    if (p1Score === winningScore) {
       isGameOver = true;
+      p1Display.classList.add("text-success");
     }
     p1Display.textContent = p1Score;
   }
@@ -31,8 +45,9 @@ p1Button.addEventListener("click", function () {
 p2Button.addEventListener("click", function () {
   if (!isGameOver) {
     p2Score += 1;
-    if (p2Score === winPoint) {
+    if (p2Score === winningScore) {
       isGameOver = true;
+      p2Display.classList.add("text-success");
     }
     p2Display.textContent = p2Score;
   }
@@ -40,7 +55,10 @@ p2Button.addEventListener("click", function () {
 
 resetButton.addEventListener("click", reset);
 
-winPointOption.addEventListener("change", function () {
-  winPoint = parseInt(this.value);
+playTo.addEventListener("change", function () {
+  winningScore = parseInt(this.value);
+  disableButton();
   reset();
 });
+
+disableButton();
